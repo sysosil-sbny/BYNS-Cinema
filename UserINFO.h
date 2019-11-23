4f13634246ddda58261e19442f11073713d1164a
seoyeonK <50603274+seoyeonK@users.noreply.github.com>
2019-11-23 19:37:21 +0900

    Update UserINFO.h

--- UserINFO.h
+++ UserINFO.h
@@ -8,19 +8,19 @@
 class User {
 public:
 	User();
-	User(std::string name, int resNum,int seatNum); //resNum=reservation number, name= name of the user
+	User(std::string name, string resNum,int seatNum); //resNum=reservation number, name= name of the user
 
 	void setName(std::string name);    //setter for name 
-	void setResNum(int resNum);        //setter for resNum
+	void setResNum(string resNum);        //setter for resNum
 	void setSeatNum(int snum);            //setter for Seat number 
 
 	std::string getName(void);        //getter for name
-	int getResNum(void);             //getter for resNum
+	string getResNum(void);             //getter for resNum
 	int getSeatNum();				//getter for Seat number 
 
 private:
 	char name[MAX_Booker_LEN + 1];   //name= name of the user
-	int resNum;                      //resNum=reservation number
+	string resNum;                      //resNum=reservation number
 	int seatNum;					 //seatNum=seat Number
 };
 
