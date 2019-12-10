92dcb1fe1e71a98460765aea9cff825feccc5d52
980926a <55770848+980926a@users.noreply.github.com>
2019-12-11 00:50:12 +0900

    Add files via upload

--- bys.cpp
+++ bys.cpp
@@ -5,16 +5,7 @@
 #include <list>
 #include <string>
 #include <sys/stat.h>
-
-#define win_abc 9
-#define win_num 9
-#define find_abc 8
-#define find_num 8
-#define black_abc 5
-#define black_num 5
-
-using namespace std;
-
+#define MAX_LENGTH 8
 #include <ncurses.h>
 
 #define COLOR_RED 1
@@ -25,9 +16,50 @@ using namespace std;
 #define COLOR_MAGENTA 5
 #define COLOR_CYAN 6
 #define COLOR_WHITE 7
-int print(void)
-{
-	int y,x;
+#define win_abc 9
+#define win_num 9
+#define find_abc 8
+#define find_num 8
+#define black_abc 5
+#define black_num 5
+
+using namespace std;
+
+//예매확인으로 넘어갈 때의 함수 
+void Bookingcheck() {
+	//(1) 고유번호 입력하기
+	cout << "자신의 예매번호를 입력하세요 : ";
+
+	string rnum;
+
+	cin >> rnum;
+
+
+
+	read_usr(rnum);
+
+}
+
+// 사용함수 선언
+void insert();
+void seat_print(list<Seat> SL, int abc, int num);
+void Booking(list<Seat> SL, int abc, int num);
+list<Seat> setting(int abc, int num);
+void menu(list<Seat> SL, int abc, int num);
+void movie();
+
+// 연결리스트 목록
+list<User> UL;
+list<Seat> winter = setting(win_abc, win_num);
+list<Seat> FM = setting(find_abc, find_num);
+list<Seat> black = setting(black_abc, black_num);
+
+
+
+
+int main(void) {
+	//print();
+int y,x;
 	initscr();
 	noecho();
 	raw();
@@ -66,13 +98,6 @@ printw("------------------------------------------------------------------------
 	attron(COLOR_PAIR(1));
 
 	//printw("RED");
-//printw("          ______ __   __ _   _  _____                _           \n" );                     
-//printw("          | ___ || | / /| | | |/  ___|              (_)                \n" );               
-//printw("          | |_/ / | V / | |_| || `--.  ______   ___  _  _ __    ___  _ __ ___    __ _ \n" );
-//printw("| ___ |  | /  | . ` | `--. ||______| / __|| || '_  | / _ || '_ ` _ '  / _` |\n" );
-//printw("| |_/ /  | |  | |-  |/|__/ /        | (__ | || | | ||  __/| | | | | || (_| |\n" );
-//printw("|____/   |_/  |_| |_||____/          .___||_||_| |_| |___||_| |_| |_| '__,_|\n" );
-
 
 	attron(COLOR_PAIR(2));
 
@@ -90,93 +115,58 @@ printw("	 | ___ \\  \\ /  | . ` | `--. \\|______| / __|| || '_ \\  / _ \\| '_ ` 
 printw("	 | |_/ /  | |  | |\\  |/\\__/ /        | (__ | || | | ||  __/| | | | | || (_| |\n" ); 
 printw("	 \\____/   \\_/  \\_| \\_/\\____/          \\___||_||_| |_| \\___||_| |_| |_| \\__,_|\n" ); 
 
-printw("\n");
-printw("\n");
-printw("\n");
+attron(COLOR_PAIR(8));
+printw("---------------------------------------------------------------------------------------------------\n");
 
-printw("                              >        1.                         < \n");
-printw("                              >        2.                         < \n");
-printw("                              >        3.                         < \n");
+	attron(COLOR_PAIR(5));
+printw("\n");
+printw("\n");
+printw("\n");
+while(1){
+	attron(COLOR_PAIR(5));
+printw("\n");
+printw("\n");
+printw("\n");
+printw("                              >        1.   Booking movie         < \n");
+printw("                              >        2.   Booking confirm       < \n");
+printw("                              >        3.   Exit program          < \n");
 	attron(COLOR_PAIR(6));
 	attron(COLOR_PAIR(7));
 
-	attron(COLOR_PAIR(8));
-printw("---------------------------------------------------------------------------------------------------\n");
-
+	
 	attrset(A_BOLD);
 	getmaxyx(stdscr,y,x);
-	//mvprintw(y-1,0,"%s","Enter to go");
-
-	refresh();return 0;
-	//getch();
-	//endwin();
-
-
 	
+	keypad(stdscr, TRUE);
+	int i = getch();
+//	if (i == 224){
+//	i=getch();
+//	case 72: printw("NO");
+	if(i==KEY_UP)
+{
+printw("                              >        Go to memu 1. Wait ...  < \n");
+sleep(2);
+movie();
+continue;
+}
+if(i==KEY_DOWN)
+{
+printw("                              >        Go to memu 2. Wait ...  < \n");
+sleep(2);
+Bookingcheck();
+continue;
 }
 
-
-//예매확인으로 넘어갈 때의 함수 
-void Bookingcheck() {
-	//(1) 고유번호 입력하기
-	cout << "자신의 예매번호를 입력하세요 : ";
-
-	//cin >> 고유번호;
-
-	//(2) 파일 open
-	/*string filepath = "./UserList.dat"; // 사용자 파일
-	int fd = open(filepath.c_str(), O_CREAT | O_APPEND | O_WRONLY, 0644); //열고
-	if (fd == -1) {
-		perror("open() error");
-	}*/
-	//(3) 정보 검색..
-
-	//(4)정보 출력
-
-	cout << "본인의 예매번호" << endl;
-	cout << "예매한 좌석 정보" << endl;
-
+if(i==KEY_RIGHT)
+{
+printw("                              >        Go to memu 3. Exit Program.  < \n");
+sleep(2);
+return 0;
 }
+//	mvprintw(y-1,0,"%s","Enter to go");
 
-// 사용함수 선언
-void insert();
-void seat_print(list<Seat> SL, int abc, int num);
-void Booking(list<Seat> SL, int abc, int num);
-list<Seat> setting(int abc, int num);
-void menu(list<Seat> SL, int abc, int num);
-void movie();
-
-// 연결리스트 목록
-list<User> UL;
-list<Seat> winter = setting(win_abc, win_num);
-list<Seat> FM = setting(find_abc, find_num);
-list<Seat> black = setting(black_abc, black_num);
-
-int main() {
-print();
-	while(1)
-	{
-		cout << "\n-----------MENU-------------" << endl;
-		cout << "1. 예매하기" << endl;
-		cout << "2. 예매확인" << endl;
-		cout << "* 끝내시려면 0을 누르세요 *\n";
-
-		int choice = 0;
-		cout << ">> ";
-		cin >> choice;
-
-		switch (choice)
-		{
-		case 0: return 0;
-		case 1: movie();
-				break;
-		case 2: Bookingcheck();
-				break;
-		default: continue;
-		}
-	}
+	refresh();
 }
-
 void movie()
 {
     while(1)
