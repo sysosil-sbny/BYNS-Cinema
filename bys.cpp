add83ef524d96b8485ca9512246f7ae2250660d1
ulming <48283598+ulming@users.noreply.github.com>
2019-12-10 10:36:36 -0800

    Add files via upload

--- bys.cpp
+++ bys.cpp
@@ -4,9 +4,17 @@
 #include <iostream>
 #include <list>
 #include <string>
-#include <unistd.h>
 #include <sys/stat.h>
-#define MAX_LENGTH 8
+
+#define win_abc 9
+#define win_num 9
+#define find_abc 8
+#define find_num 8
+#define black_abc 5
+#define black_num 5
+
+using namespace std;
+
 #include <ncurses.h>
 
 #define COLOR_RED 1
@@ -17,51 +25,11 @@
 #define COLOR_MAGENTA 5
 #define COLOR_CYAN 6
 #define COLOR_WHITE 7
-#define win_abc 9
-#define win_num 9
-#define find_abc 8
-#define find_num 8
-#define black_abc 5
-#define black_num 5
-
-using namespace std;
-
-
-// 사용함수 선언
-void insert();
-void seat_print(list<Seat> SL, int abc, int num);
-void Booking(list<Seat> SL, int abc, int num);
-list<Seat> setting(int abc, int num);
-void menu(list<Seat> SL, int abc, int num);
-void movie();
-string randchar();
-void read_usr(string resNum);
-void write_usr(string name, string resnum, int seatnum);
-
-// 연결리스트 목록
-list<User> UL;
-list<Seat> winter = setting(win_abc, win_num);
-list<Seat> FM = setting(find_abc, find_num);
-list<Seat> black = setting(black_abc, black_num);
-
-
-//예매확인으로 넘어갈 때의 함수 
-void Bookingcheck() {
-    //(1) 고유번호 입력하기
-    cout << "자신의 예매번호를 입력하세요 : ";
- 
-    string rnum;
- 
-	cin >> rnum;
- 
-    read_usr(rnum);
- 
-}
-
-int main(void) {
-	//print();
-int y,x;
+int print(void)
+{
+	int y,x;
 	initscr();
+	noecho();
 	raw();
 //01
 	if(has_colors() ==FALSE)
@@ -91,13 +59,20 @@ int y,x;
 	init_pair(7,COLOR_CYAN,COLOR_MAGENTA);
 
 //04
-	//attron(A_BOLD);
+	attron(A_BOLD);
 //05
 attron(COLOR_PAIR(8));
 printw("----------------------------------------------------------------------------------------------------\n");
 	attron(COLOR_PAIR(1));
 
 	//printw("RED");
+//printw("          ______ __   __ _   _  _____                _           \n" );                     
+//printw("          | ___ || | / /| | | |/  ___|              (_)                \n" );               
+//printw("          | |_/ / | V / | |_| || `--.  ______   ___  _  _ __    ___  _ __ ___    __ _ \n" );
+//printw("| ___ |  | /  | . ` | `--. ||______| / __|| || '_  | / _ || '_ ` _ '  / _` |\n" );
+//printw("| |_/ /  | |  | |-  |/|__/ /        | (__ | || | | ||  __/| | | | | || (_| |\n" );
+//printw("|____/   |_/  |_| |_||____/          .___||_||_| |_| |___||_| |_| |_| '__,_|\n" );
+
 
 	attron(COLOR_PAIR(2));
 
@@ -115,60 +90,95 @@ printw("	 | ___ \\  \\ /  | . ` | `--. \\|______| / __|| || '_ \\  / _ \\| '_ ` 
 printw("	 | |_/ /  | |  | |\\  |/\\__/ /        | (__ | || | | ||  __/| | | | | || (_| |\n" ); 
 printw("	 \\____/   \\_/  \\_| \\_/\\____/          \\___||_||_| |_| \\___||_| |_| |_| \\__,_|\n" ); 
 
-attron(COLOR_PAIR(8));
-printw("---------------------------------------------------------------------------------------------------\n");
+printw("\n");
+printw("\n");
+printw("\n");
 
-	attron(COLOR_PAIR(5));
-printw("\n");
-printw("\n");
-printw("\n");
-while(1){
-	attron(COLOR_PAIR(5));
-printw("\n");
-printw("\n");
-printw("\n");
-printw("                              >        1.   Booking movie         < \n");
-printw("                              >        2.   Booking confirm       < \n");
-printw("                              >        3.   Exit program          < \n");
+printw("                              >        1.                         < \n");
+printw("                              >        2.                         < \n");
+printw("                              >        3.                         < \n");
 	attron(COLOR_PAIR(6));
 	attron(COLOR_PAIR(7));
 
-	
+	attron(COLOR_PAIR(8));
+printw("---------------------------------------------------------------------------------------------------\n");
+
 	attrset(A_BOLD);
 	getmaxyx(stdscr,y,x);
+	//mvprintw(y-1,0,"%s","Enter to go");
+
+	refresh();return 0;
+	//getch();
+	//endwin();
+
+
 	
-	keypad(stdscr, TRUE);
-	int i = getch();
-//	if (i == 224){
-//	i=getch();
-//	case 72: printw("NO");
-	if(i==KEY_UP)
-{
-printw("                              >        Go to memu 1. Wait ...  < \n");
-sleep(2);
-movie();
-continue;
-}
-if(i==KEY_DOWN)
-{
-printw("                              >        Go to memu 2. Wait ...  < \n");
-sleep(2);
-Bookingcheck();
-continue;
 }
 
-if(i==KEY_RIGHT)
-{
-printw("                              >        Go to memu 3. Exit Program.  < \n");
-sleep(2);
-return 0;
-}
-//	mvprintw(y-1,0,"%s","Enter to go");
 
-	refresh();
+//예매확인으로 넘어갈 때의 함수 
+void Bookingcheck() {
+	//(1) 고유번호 입력하기
+	printw("자신의 예매번호를 입력하세요 : ");
+
+	//cin >> 고유번호;
+
+	//(2) 파일 open
+	/*string filepath = "./UserList.dat"; // 사용자 파일
+	int fd = open(filepath.c_str(), O_CREAT | O_APPEND | O_WRONLY, 0644); //열고
+	if (fd == -1) {
+		perror("open() error");
+	}*/
+	//(3) 정보 검색..
+
+	//(4)정보 출력
+
+	printw ("본인의 예매번호\n");
+	printw("예매한 좌석 정보\n");
+
 }
+
+// 사용함수 선언
+void insert();
+void seat_print(list<Seat> SL, int abc, int num);
+void Booking(list<Seat> SL, int abc, int num);
+list<Seat> setting(int abc, int num);
+void menu(list<Seat> SL, int abc, int num);
+void movie();
+
+// 연결리스트 목록
+list<User> UL;
+list<Seat> winter = setting(win_abc, win_num);
+list<Seat> FM = setting(find_abc, find_num);
+list<Seat> black = setting(black_abc, black_num);
+
+int main() {
+print();
+	while(1)
+	{
+        printw("\n-----------MENU-------------\n");
+		printw("1. 예매하기\n");
+		printw("2. 예매확인\n");
+		printw("* 끝내시려면 0을 누르세요 *\n");
+
+		int choice = 0;
+		printw(">> ");
+		scanw("%d", &choice);
+
+		switch (choice)
+		{
+		case 0: return 0;
+		case 1: movie();
+				break;
+		case 2: Bookingcheck();
+				break;
+		default: continue;
+		}
+	}
 }
-void movie(){
+
+void movie()
+{
     while(1)
     {
         printw("\n---------- MOVIE ----------\n");
@@ -179,7 +189,7 @@ void movie(){
 
         int movie = 0;
         printw(">> ");
-        scanw("%d",&movie);
+        scanw("%d", &movie);
 
         switch (movie)
         {
@@ -200,8 +210,10 @@ string name()
 	//예매한 뒤에 이름 입력. 입력받고 고유함수로 저장
 	string usrname;
 	printw("이름을 입력하세요: ");
-	scanw("%s",&usrname);
+	scanw("%s", &usrname);
 
 	return usrname;
 }
+
+
 
